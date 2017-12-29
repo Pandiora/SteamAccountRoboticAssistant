2725c8c7742e397697abed106c2a0ebe570a18b0
Pandi <Pandiora@users.noreply.github.com>
2017-12-29 17:50:51 +0100

    Fix licenseBulkActivation getting stuck
    Due to our custom logout being to fast, it can fail on reloading the page while changing to login and instead just refreshing the store-page for given appid. Fix should send you back to login where it gets executed further if there accounts left which need the license for the given appid.

--- js/licenseBulkActivation.js
+++ js/licenseBulkActivation.js
@@ -13,23 +13,28 @@ chrome.runtime.sendMessage({greeting: 'getLicenseBulkActivationStatus'}, functio
           chrome.runtime.sendMessage({greeting: 'setLicenseBulkActivationInactive'});
         }
       }, 1500);
-    } else if(document.location.href.indexOf("http://store.steampowered.com/app/"+res.appid+"/") !== -1 && (jQuery('#account_pulldown').text() !== "")){
-      if($('.already_in_library')[0]){
-        // User already owns this game >> set skip for this user and log out
-        var user = $('#account_pulldown').text();
-        chrome.runtime.sendMessage({
-          greeting: 'setSkipForLogin',
-          user: user
-        }, function(response) {
-          if (response == 1) {
-            $.post('https://store.steampowered.com/logout/', {
-              sessionid: sessionID
-            });
-            document.location.href = "https://store.steampowered.com/login/?redir=app%2F"+res.appid+"%2F";
-          }
-        });
+    } else if(document.location.href.indexOf("http://store.steampowered.com/app/"+res.appid+"/") !== -1){
+      if(jQuery('#account_pulldown').text() !== ""){
+        if($('.already_in_library')[0]){
+          // User already owns this game >> set skip for this user and log out
+          var user = $('#account_pulldown').text();
+          chrome.runtime.sendMessage({
+            greeting: 'setSkipForLogin',
+            user: user
+          }, function(response) {
+            if (response == 1) {
+              $.post('https://store.steampowered.com/logout/', {
+                sessionid: sessionID
+              });
+              document.location.href = "https://store.steampowered.com/login/?redir=app%2F"+res.appid+"%2F";
+            }
+          });
+        } else {
+          $('#game_area_purchase').find('.btnv6_green_white_innerfade')[0].click();
+        }
       } else {
-        $('#game_area_purchase').find('.btnv6_green_white_innerfade')[0].click();
+        // Sometimes the logout is to fast and the script could get stuck - go to login-page
+        document.location.href = "https://store.steampowered.com/login/?redir=app%2F"+res.appid+"%2F";
       }
     } else if(document.location.href == "http://store.steampowered.com/checkout/addfreelicense/"){
       // Alright we added the free license - set skip-flag for this user and log out
